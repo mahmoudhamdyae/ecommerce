65dd02f25e87c80ff6ad32c9118c8c8658c29c99
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-12-26 15:43:48 +0200

    Cart Icon

--- lib/presentation/widgets/cart_icon.dart
+++ lib/presentation/widgets/cart_icon.dart
@@ -8,10 +8,7 @@ class CartIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return
-
-
-      badges.Badge(
+    return badges.Badge(
         position: badges.BadgePosition.topStart(top: -10, start: -12),
         badgeContent: const Padding(
           padding: EdgeInsets.only(top: 4.0),
@@ -27,51 +24,6 @@ class CartIcon extends StatelessWidget {
           color: ColorManager.white,
           size: 24,
         ),
-      )
-
-
-      // badges.Badge(
-      //   position: badges.BadgePosition.topEnd(top: -10, end: -12),
-      //   showBadge: true,
-      //   ignorePointer: false,
-      //   onTap: () {},
-      //   badgeContent:
-      //   Icon(Icons.check, color: Colors.white, size: 10),
-      //   badgeAnimation: badges.BadgeAnimation.rotation(
-      //     animationDuration: Duration(seconds: 1),
-      //     colorChangeAnimationDuration: Duration(seconds: 1),
-      //     loopAnimation: false,
-      //     curve: Curves.fastOutSlowIn,
-      //     colorChangeAnimationCurve: Curves.easeInCubic,
-      //   ),
-      //   badgeStyle: badges.BadgeStyle(
-      //     shape: badges.BadgeShape.square,
-      //     badgeColor: Colors.blue,
-      //     padding: EdgeInsets.all(5),
-      //     borderRadius: BorderRadius.circular(4),
-      //     borderSide: BorderSide(color: Colors.white, width: 2),
-      //     borderGradient: badges.BadgeGradient.linear(
-      //         colors: [Colors.red, Colors.black]),
-      //     badgeGradient: badges.BadgeGradient.linear(
-      //       colors: [Colors.blue, Colors.yellow],
-      //       begin: Alignment.topCenter,
-      //       end: Alignment.bottomCenter,
-      //     ),
-      //     elevation: 0,
-      //   ),
-      //   child: Text('Badge'),
-      // )
-
-    //   IconButton(
-    //     onPressed: () {},
-    //     icon: const Icon(
-    //         Icons.shopping_cart,
-    //         color: ColorManager.white
-    //     )
-    // )
-
-
-
-    ;
+      );
   }
 }
