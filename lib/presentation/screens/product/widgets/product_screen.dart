3b1ca1392495ec100be5a49827bacd7eea06cd8d
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-12-27 14:46:20 +0200

    Product Screen Texts

--- lib/presentation/screens/product/widgets/product_screen.dart
+++ lib/presentation/screens/product/widgets/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/presentation/resources/strings_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 
 import '../../../resources/color_manager.dart';
@@ -77,12 +78,52 @@ class ProductScreen extends StatelessWidget {
               ),
               Expanded(
                 child: Container(
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: ColorManager.white,
                     borderRadius: BorderRadius.all(Radius.circular(AppSize.borderRadius))
                   ),
+                  child: Padding(
+                    padding: EdgeInsets.all(AppPadding.mediumPadding),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Expanded(
+                              child: Text(
+                                'كمبروسر هواء 200 لتر 4 حصان سير اندكشن موتور ملفات نحاس - EACPB40203',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600
+                                ),
+                              ),
+                            ),
+                            RatingBar.builder(
+                              initialRating: 0,
+                              minRating: 0,
+                              updateOnDrag: false,
+                              ignoreGestures: true,
+                              direction: Axis.horizontal,
+                              allowHalfRating: true,
+                              itemCount: 5,
+                              itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                              itemSize: 18,
+                              itemBuilder: (context, _) => const Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              onRatingUpdate: (rating) {},
+                            ),
+                          ],
+                        ),
+                        const Text(
+                            'كمبروسر'
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
