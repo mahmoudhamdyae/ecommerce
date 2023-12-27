a20d88ca471e3323a30ab56298c450232cc256a2
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-12-27 11:34:36 +0200

    Best Seller

--- lib/presentation/screens/home/widgets/home_screen_body.dart
+++ lib/presentation/screens/home/widgets/home_screen_body.dart
@@ -1,7 +1,12 @@
+import 'package:ecommerce/presentation/resources/color_manager.dart';
+import 'package:ecommerce/presentation/resources/font_manager.dart';
+import 'package:ecommerce/presentation/resources/strings_manager.dart';
 import 'package:ecommerce/presentation/resources/values_manager.dart';
 import 'package:ecommerce/presentation/screens/home/widgets/products_list.dart';
 import 'package:ecommerce/presentation/screens/home/widgets/swiper.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeScreenBody extends StatelessWidget {
 
@@ -15,6 +20,38 @@ class HomeScreenBody extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(vertical: AppPadding.mediumPadding),
           child: ProductsList(),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // الأكثر مبيعا
+            Padding(
+              padding: const EdgeInsets.all(AppPadding.mediumPadding),
+              child: Text(
+                AppStrings.bestSeller.tr,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: FontSize.s16
+                ),
+              ),
+            ),
+            // إظهار المزيد
+            Expanded(child: Container()),
+            Text(
+              AppStrings.showMore.tr,
+              style: const TextStyle(
+                color: ColorManager.grey
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                },
+                icon: const Icon(
+                  Icons.arrow_forward_ios,
+                  color: ColorManager.yellow,
+                )
+            )
+          ],
         ),
       ],
     );
