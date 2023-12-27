7dbe9c31de3933228fe7b059d164a35efc4d7ef4
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-12-27 09:28:45 +0200

    Require Auth Dialog

--- lib/presentation/widgets/dialogs/require_auth_dialog.dart
+++ lib/presentation/widgets/dialogs/require_auth_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/presentation/resources/color_manager.dart';
+import 'package:ecommerce/presentation/resources/font_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -19,23 +20,59 @@ showRequireAuthDialog(BuildContext context) {
           title: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              const Icon(Icons.logout_outlined, color: ColorManager.primary,),
-              Text(AppStrings.pleaseLogIn.tr),
+              const Icon(
+                Icons.logout_outlined,
+                color: ColorManager.primary,
+                size: 50,
+              ),
+              const SizedBox(height: AppSize.s8,),
+              Text(
+                AppStrings.pleaseLogIn.tr,
+                style: const TextStyle(
+                  fontSize: FontSize.s16,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
               const SizedBox(height: AppSize.s16,),
               Row(
                 children: [
-                  TextButton(
-                      onPressed: () {
-                        Get.back();
-                        Get.to(const LoginScreen());
-                      },
-                      child: Text(AppStrings.loginContinueButton.tr)
+                  Expanded(
+                    child: FilledButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(AppSize.borderRadius)),
+                              )
+                          ),
+                          backgroundColor: MaterialStateProperty.all(ColorManager.primary),
+                        ),
+                        onPressed: () {
+                          Get.back();
+                          Get.to(const LoginScreen());
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: AppPadding.mediumPadding),
+                          child: Text(AppStrings.loginContinueButton.tr),
+                        )
+                    ),
                   ),
-                  TextButton(
+                  const SizedBox(width: AppSize.s8,),
+                  FilledButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(AppSize.borderRadius)),
+                            )
+                        ),
+                        backgroundColor: MaterialStateProperty.all(ColorManager.grey),
+                      ),
                       onPressed: () {
                         Navigator.of(context).pop();
                       },
-                      child: Text(AppStrings.cancel.tr)
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: AppPadding.mediumPadding),
+                        child: Text(AppStrings.cancel.tr),
+                      )
                   ),
                 ],
               )
