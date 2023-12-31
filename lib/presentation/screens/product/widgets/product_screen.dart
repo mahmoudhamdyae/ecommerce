385798f171d6d64df25eb57a7ebfc42803bb4bf2
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-12-31 13:52:33 +0200

    Product Screen Bottom

--- lib/presentation/screens/product/widgets/product_screen.dart
+++ lib/presentation/screens/product/widgets/product_screen.dart
@@ -18,113 +18,125 @@ class ProductScreen extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        body: Container(
-          color: ColorManager.lightGrey,
-          child: ListView(
-            children: [
-              ProductTopBar(),
-              Container(
-                height: 200,
-                color: ColorManager.grey,
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                    color: ColorManager.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(AppSize.borderRadius),
-                        topRight: Radius.circular(AppSize.borderRadius))),
-                child: Padding(
-                  padding: const EdgeInsets.all(AppPadding.mediumPadding),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+        body: Stack(
+          children: [
+            Container(
+              color: ColorManager.lightGrey,
+              padding: const EdgeInsets.only(bottom: 75),
+              child: ListView(
+                children: [
+                  ProductTopBar(),
+                  Container(
+                    height: 200,
+                    color: ColorManager.grey,
+                  ),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: ColorManager.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(AppSize.borderRadius),
+                            topRight: Radius.circular(AppSize.borderRadius))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(AppPadding.mediumPadding),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          const Expanded(
-                            child: Text(
-                              'كمبروسر هواء 200 لتر 4 حصان سير اندكشن موتور ملفات نحاس - EACPB40203',
-                              style: TextStyle(fontWeight: FontWeight.w600),
-                            ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Expanded(
+                                child: Text(
+                                  'كمبروسر هواء 200 لتر 4 حصان سير اندكشن موتور ملفات نحاس - EACPB40203',
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              RatingBar.builder(
+                                initialRating: 0,
+                                minRating: 0,
+                                updateOnDrag: false,
+                                ignoreGestures: true,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemPadding:
+                                    const EdgeInsets.symmetric(horizontal: 4.0),
+                                itemSize: 18,
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) {},
+                              ),
+                            ],
                           ),
-                          RatingBar.builder(
-                            initialRating: 0,
-                            minRating: 0,
-                            updateOnDrag: false,
-                            ignoreGestures: true,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 5,
-                            itemPadding:
-                                const EdgeInsets.symmetric(horizontal: 4.0),
-                            itemSize: 18,
-                            itemBuilder: (context, _) => const Icon(
-                              Icons.star,
-                              color: Colors.amber,
-                            ),
-                            onRatingUpdate: (rating) {},
+                          const SizedBox(
+                            height: AppPadding.smallPadding,
                           ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: AppPadding.smallPadding,
-                      ),
-                      const Text(
-                        'متوفر 3 قطع',
-                        style: TextStyle(
-                          color: ColorManager.red,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: AppPadding.smallPadding,
-                      ),
-                      const Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            '28661 EGP',
+                          const Text(
+                            'متوفر 3 قطع',
                             style: TextStyle(
                               color: ColorManager.red,
                               fontWeight: FontWeight.w500,
                             ),
                           ),
-                          SizedBox(
-                            width: AppPadding.smallPadding,
+                          const SizedBox(
+                            height: AppPadding.smallPadding,
+                          ),
+                          const Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                '28661 EGP',
+                                style: TextStyle(
+                                  color: ColorManager.red,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              SizedBox(
+                                width: AppPadding.smallPadding,
+                              ),
+                              Text(
+                                '28661 EGP',
+                                style: TextStyle(
+                                    color: ColorManager.grey,
+                                    decoration: TextDecoration.lineThrough),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: AppSize.s16,
                           ),
                           Text(
-                            '28661 EGP',
+                            AppStrings.productDetails.tr,
+                            style: const TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(
+                            height: AppSize.s8,
+                          ),
+                          const Text(
+                            'الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت ',
                             style: TextStyle(
-                                color: ColorManager.grey,
-                                decoration: TextDecoration.lineThrough),
+                                fontWeight: FontWeight.w400,
+                                color: ColorManager.grey),
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: AppSize.s16,
-                      ),
-                      Text(
-                        AppStrings.productDetails.tr,
-                        style: const TextStyle(fontWeight: FontWeight.w600),
-                      ),
-                      const SizedBox(
-                        height: AppSize.s8,
-                      ),
-                      const Text(
-                        'الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت الجهد: 220 فولت ',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            color: ColorManager.grey),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  const ProductTabBar(),
+                  const ProductTabs(),
+                  SimilarProducts(),
+                ],
               ),
-              const ProductTabBar(),
-              const ProductTabs(),
-              SimilarProducts(),
-            ],
-          ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+                child: Container(
+                  color: ColorManager.white,
+                  height: 75,
+                )
+            )
+          ],
         ),
       ),
     );
